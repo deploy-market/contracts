512360e546d882b7bff4717ac3a3095be339493c
JaniAnttonen <jani.anttonen@protonmail.ch>
2023-10-16 21:34:09 +0300

    Add makefile for helping tests

---
+++ contracts/src/Transactions.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.19;
+
+import "./interfaces/DeployEligible.sol";
+
+contract DeployEscrow {
+    struct Escrow {
+        uint256 amount;
+        address submitter;
+        uint256 deadline;
+    }
+
+    // Mapping from target hash (contract address + secret) to escrow
+    mapping(bytes32 => Escrow) public escrows;
+
+    /**
+     * A deploy request is simply a customer submitted challenge submitted to
+     * this escrow that can be withdrawn with successful deployment of a contract
+     */
+    function submitRequest(
+        bytes32 targetHash,
+        address submitter,
+        uint256 deadline
+    ) public payable {
+        escrows[targetHash] = Escrow({
+            amount: msg.value,
+            submitter: submitter,
+            deadline: deadline
+        });
+    }
+
+    /**
+     * @dev Withdraws the escrowed funds if the target contract has been deployed
+     * successfully.
+     */
+    function reward(
+        address targetAddress,
+        address payable deployerAddress
+    ) public {
+        // Check that the target contract has been deployed successfully
+        DeployEligible target = DeployEligible(address(targetAddress));
+        bytes32 secret = target.getDeploySecret();
+
+        // (Re)construct the submitted target hash from deployed contract info
+        bytes32 targetHash = keccak256(abi.encodePacked(targetAddress, secret));
+
+        // Check that the escrow exists and has not expired
+        Escrow memory escrow = escrows[targetHash];
+        require(escrow.amount > 0, "No escrow exists for this target");
+        require(escrow.deadline > block.number, "Job deadline has passed");
+
+        // Transfer marked funds to the deployer
+        delete escrows[targetHash];
+        (bool success, ) = deployerAddress.call{value: escrow.amount}("");
+        require(success, "Failed to send a reward");
+    }
+
+    /**
+     * @dev Withdraws the escrowed funds if the deadline has passed without a successful deployment
+     */
+    function withdraw(bytes32 targetHash) public {
+        Escrow memory escrow = escrows[targetHash];
+        require(escrow.amount > 0, "No escrow exists for this target");
+        require(
+            escrow.deadline < block.number,
+            "Job still open, wait for the deadline"
+        );
+
+        delete escrows[targetHash];
+        (bool success, ) = escrow.submitter.call{value: escrow.amount}("");
+        require(success, "Failed to withdraw");
+    }
+}
--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -1,8 +1,45 @@
+"use client"
 import Wrapper from '@/components/wrapper'
-
+import { createOrbitDB } from '@orbitdb/core'
+import IPFS from 'ipfs-core'
 import Image from 'next/image'
+import { useEffect } from "react"
 
 export default function Home() {
+  const connect = async () => {
+    const ipfs = await IPFS.create()
+    const orbitdb = await createOrbitDB({ ipfs })
+    // Create / Open a database. Defaults to db type "events".
+    const db = await orbitdb.open("hello")
+    
+    const address = db.address
+    console.log(address)
+    // "/orbitdb/zdpuAkstgbTVGHQmMi5TC84auhJ8rL5qoaNEtXo2d5PHXs2To"
+    // The above address can be used on another peer to open the same database
+
+    // Listen for updates from peers
+    db.events.on("update", async (entry: any) => {
+      console.log(entry)
+      const all = await db.all()
+      console.log(all)
+    })
+
+    // Add an entry
+    const hash = await db.add("world")
+    console.log(hash)
+
+    // Query
+    for await (const record of db.iterator()) {
+      console.log(record)
+    }
+    
+    await db.close()
+    await orbitdb.stop()
+  }
+
+  useEffect(() => {
+    connect()
+  }, [])
   return (
       <main className="flex items-center justify-center h-screen w-full overflow-hidden text-gray-400 p-2">
         <Wrapper>
